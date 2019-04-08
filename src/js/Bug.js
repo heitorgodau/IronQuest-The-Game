a6249ecccd768610616cb9c025236bf173bed23f
Heitor Godau <heitorgodau@gmail.com>
2019-04-08 17:36:14 -0300

    The enemies are moving!

--- src/js/Bug.js
+++ src/js/Bug.js
@@ -1,11 +1,21 @@
+const bugStore = []
+
 class Bug{
   constructor(){
-    this.img = Math.floor(Math.random() * 16777216).toString(16);
-    this.size = 50;
-    this.posX = (gameScreen.width/2) - this.size/2;
-    this.posY = (gameScreen.height/2) - this.size/2;
+    this.img = `#${Math.floor(Math.random() * 16777216).toString(16)}`;
+    this.size = 25;
+    this.posX = Math.floor(Math.random() * (gameScreen.width - this.size));
+    this.posY = 0 - this.size * 2;
     this.hp = 15;
     this.str = this.hp/3;
-    this.speed = this.size/2;
+    this.speed = 1;
+  }
+  draw(){
+    this.posY += this.speed;
+    ctx.fillStyle = this.img;
+    ctx.beginPath();
+    ctx.arc(this.posX, this.posY, this.size, 0, Math.PI*2);
+    ctx.fill();
+    ctx.closePath();
   }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,29 @@ const gameScreen = document.getElementById('game-screen');
 const ctx = gameScreen.getContext('2d');
 const clear = () => ctx.clearRect(0,0, gameScreen.width, gameScreen.height);
 
+let frames = 0;
 let hero = new Hero('Iron');
+let bugs = new Bug();
 
 window.addEventListener('keydown', event =>  {
   hero.moveHero(event)
   hero.speedUp(event);
 });
 
+const createBugs = () => {
+  if(frames % 120 === 0){
+    bugStore.push(new Bug);
+  }
+};
+
+const drawBugs = () => bugStore.forEach( enemy => enemy.draw());
+
 const animate = () => {
   clear();
   hero.drawHero();
-  hero.drawHero()
+  createBugs();
+  drawBugs();
+  frames += 1;
   window.requestAnimationFrame(animate);
 }
 
