6812c3455aba9afd2adbd3b16e4101bceafa60f1
Heitor <heitorgodau@gmail.com>
2019-08-28 10:22:41 -0300

    Font style changed

--- src/js/Hero.js
+++ src/js/Hero.js
@@ -1,30 +1,32 @@
-class Hero{
-  constructor(name){
+class Hero {
+  constructor(name) {
     this.name = name;
     this.img = new Image();
     this.size = 50;
-    this.posX = (gameScreen.width/2) - this.size/2;
-    this.posY = (gameScreen.height/2) - this.size/2;
+    this.posX = (gameScreen.width / 2) - this.size / 2;
+    this.posY = (gameScreen.height / 2) - this.size / 2;
     this.hp = 15;
     this.str = 20;
-    this.speed = this.size/2;
-    this.score = 0
-    //this.attackImg = new Image();
-    this.direction = 'up'
+    this.speed = this.size / 2;
+    this.score = 0;
+    this.direction = 'up';
   }
-  getScore(){
-    ctx.font = '30px Impact'
-    ctx.fillStyle = '#72BF8C'
-    ctx.fillText(`Score: ${this.score}`, 180, 40)    
+
+  getHP() {
+    ctx.font = '20px Pixel';
+    ctx.fillStyle = 'tomato';
+    ctx.fillText(`HP: ${this.hp}`, 20, 40);
   }
-  getHP(){
-    ctx.font = '30px Impact'
-    ctx.fillStyle = 'tomato'
-    ctx.fillText(`HP: ${this.hp}pts`, 20, 40)
+
+  getScore() {
+    ctx.font = '20px Pixel';
+    ctx.fillStyle = '#72BF8C';
+    ctx.fillText(`Score: ${this.score}`, 125, 40);
   }
-  drawHero(){
+
+  drawHero() {
     this.img.src = 'src/img/hero-positions.png';
-    switch(this.direction){
+    switch (this.direction) {
       case 'up':
         ctx.drawImage(this.img, 96, 0, 32, 32, this.posX, this.posY, this.size, this.size);
         break;
@@ -32,50 +34,54 @@ class Hero{
         ctx.drawImage(this.img, 64, 0, 32, 32, this.posX, this.posY, this.size, this.size);
         break;
       case 'down':
-        ctx.drawImage(this.img, 0, 0, 32, 32, this.posX, this.posY, this.size, this.size);        
+        ctx.drawImage(this.img, 0, 0, 32, 32, this.posX, this.posY, this.size, this.size);
         break;
       case 'left':
         ctx.drawImage(this.img, 32, 0, 32, 32, this.posX, this.posY, this.size, this.size);
         break;
+      default:
+        ctx.drawImage(this.img, 96, 0, 32, 32, this.posX, this.posY, this.size, this.size);
     }
   }
-  speedUp(e){
-    e.shiftKey ? this.speed = this.size : this.speed = this.size/2;
+
+  speedUp(e) {
+    e.shiftKey ? this.speed = this.size : this.speed = this.size / 2;
   }
 
-  moveHero(e){
-    switch(e.keyCode){
+  moveHero(e) {
+    switch (e.keyCode) {
       case 37:
-        this.posX > 0 ? this.posX -= this.speed : this.posX = 0;
+        this.posX - this.speed > 0 ? this.posX -= this.speed : this.posX = 0;
         this.direction = 'left';
         break;
       case 38:
-        this.posY > 0 ? this.posY -= this.speed : this.posY = 0;
+        this.posY - this.speed > 0 ? this.posY -= this.speed : this.posY = 0;
         this.direction = 'up';
         break;
       case 39:
-        this.posX < 750 ? this.posX += this.speed : this.posX = 750;
+        this.posX + this.speed < 750 ? this.posX += this.speed : this.posX = 750;
         this.direction = 'right';
         break;
       case 40:
-        this.posY < 450 ? this.posY += this.speed : this.posY = 450;
+        this.posY + this.speed < 450 ? this.posY += this.speed : this.posY = 450;
         this.direction = 'down';
         break;
+      default:
+        console.log(`${e.key} is not a valid key!`);
     }
   }
-  heroHit(enemy){
-    return (this.posX < enemy.posX + enemy.size) && (this.posX + this.size > enemy.posX) && (this.posY < enemy.posY + enemy.size) && (this.posY + this.size > enemy.posY);
+
+  heroHit(enemy) {
+    return (this.posX < enemy.posX + enemy.size)
+      && (this.posX + this.size > enemy.posX)
+      && (this.posY < enemy.posY + enemy.size)
+      && (this.posY + this.size > enemy.posY);
   }
-  attack(enemy){
-    return (this.posX - this.size / 2 < enemy.posX + enemy.size) && (this.posX + this.size * 1.5 > enemy.posX) && (this.posY - this.size / 2 < enemy.posY + enemy.size) && (this.posY + this.size * 1.5 > enemy.posY);
+
+  attack(enemy) {
+    return (this.posX - this.size / 2 < enemy.posX + enemy.size)
+    && (this.posX + this.size * 1.5 > enemy.posX)
+    && (this.posY - this.size / 2 < enemy.posY + enemy.size)
+    && (this.posY + this.size * 1.5 > enemy.posY);
   }
-  /* attackAnimation(){
-     let x = 0;
-     this.attackImg.src = 'src/img/attack-sprite-sheet.png';
-     while( x <= 300 ){
-       clear();
-       ctx.drawImage(this.attackImg, x, 0, 50, 50, this.posX, this.posY, 50, 50);
-       x += 50;
-     }
-   }*/
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 const gameScreen = document.getElementById('game-screen');
 const ctx = gameScreen.getContext('2d');
-const gameOverSound = new Audio('src/audio/geme-over.mp3')
+const gameOverSound = new Audio('src/audio/geme-over.mp3');
 const bgSound = new Audio('src/audio/bg-sound2.mp3');
 const heroAttackSound = new Audio('src/audio/hero-attack.mp3');
 const bugAttackSound = new Audio('src/audio/bug-attack.mp3');
@@ -11,56 +11,55 @@ let frames = 0;
 let pause = true;
 let hero = new Hero('Iron');
 
-window.addEventListener('keydown', event =>  {
-  hero.moveHero(event)
+window.addEventListener('keydown', (event) => {
+  hero.moveHero(event);
   hero.speedUp(event);
   pauseGame(event);
-  if(event.keyCode === 17){
-    //hero.attackAnimation();
+  if (event.keyCode === 17) {
     bugDamage();
   }
 });
 
 const changeTitleColor = () => {
-  if(frames % 60 === 0){
+  if (frames % 60 === 0) {
     document.querySelector('.title h1').style.color = `#${Math.floor(Math.random() * 16777216).toString(16)}`;
   }
 };
 
-const clear = () => ctx.clearRect(0,0, gameScreen.width, gameScreen.height);
+const clear = () => ctx.clearRect(0, 0, gameScreen.width, gameScreen.height);
 
-const pauseGame = event => {
-  if (event.keyCode === 13){
-    ctx.font = '120px Impact'
-    ctx.fillStyle = '#A1BDF0'
-    ctx.fillText('PAUSE', 250, 300);
+const pauseGame = (event) => {
+  if (event.keyCode === 13) {
+    ctx.font = '80px Pixel';
+    ctx.fillStyle = '#A1BDF0';
+    ctx.fillText('PAUSE', 210, 300);
     bgSound.pause();
     return pause = !pause;
   }
-}
+};
 
 const resetGame = () => {
-  bugStore.splice(0,bugStore.length);
+  bugStore.splice(0, bugStore.length);
   frames = 0;
   hero = new Hero();
-}
+};
 
 const gameOver = () => {
   gameOverSound.play();
   pause = true;
   clear();
-  ctx.font = '120px Impact'
-  ctx.fillStyle = '#C73E1D'
-  ctx.fillText('GAME OVER', 125, 275);
-  ctx.font = '80px Impact'
-  ctx.fillStyle = '#72BF8C'
-  ctx.fillText(`Your Score: ${hero.score}`, 110, 375);
+  ctx.font = '60px Pixel';
+  ctx.fillStyle = '#C73E1D';
+  ctx.fillText('GAME OVER', 125, 250);
+  ctx.font = '40px Pixel';
+  ctx.fillStyle = '#72BF8C';
+  ctx.fillText(`Your Score: ${hero.score}`, 125, 325);
   resetGame();
 };
 
-////////ENGINE
+// ENGINE
 const animate = () => {
-  if(pause === false){
+  if (pause === false) {
     clear();
     hero.drawHero();
     createBugs();
@@ -75,6 +74,6 @@ const animate = () => {
   frames += 1;
   changeTitleColor();
   window.requestAnimationFrame(animate);
-}
+};
 
-  animate();
+animate();
